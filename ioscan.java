df3041e95c5d6ad00fc51e6b0c2a72d1412dc1cb
KARAN <110051623+Karanloganathan@users.noreply.github.com>
2023-01-08 13:41:30 +0530

    Create ioscan.java

---
+++ ioscan.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+import java.util.*;
+public class ioscan{
+    public static void main(String[] args){
+         Scanner sc=new Scanner(System.in);
+         //Getting a int variable
+        System.out.println("Enter the id A");
+        int a=sc.nextInt();
+        System.out.println("the value is"+ a); 
+        System.out.println("........");
+        Scanner ss=new Scanner(System.in);
+        
+        //for string
+        System.out.println("Enter your  name");
+        String b=ss.nextLine();
+        System.out.println("the name is "+ b); 
+        System.out.println("........");
+        
+        //for Datatype double
+        Scanner sss=new Scanner(System.in);
+        System.out.println("Enter your percentage");
+        double c=sss.nextDouble();
+        System.out.println("the value is "+ c);
+        
+        //for getting a character
+        char cv=sss.next().charAt(0);
+        System.out.println(" the character is"+cv); */
+        }
+}
