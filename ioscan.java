d78850a45f6361137361d5fa63cc4c202abbcefd
KARAN <110051623+Karanloganathan@users.noreply.github.com>
2023-01-10 21:34:56 +0530

    Update ioscan.java

--- ioscan.java
+++ ioscan.java
@@ -9,20 +9,16 @@ public class ioscan{
         System.out.println("the value is"+ a); 
         
         Scanner ss=new Scanner(System.in);
-        
         //for string
         System.out.println("Enter your  name");
         String b=ss.nextLine();
         System.out.println("the name is "+ b); 
-       
-        
-        //for Datatype double
+         //for Datatype double
         Scanner sss=new Scanner(System.in);
         System.out.println("Enter your percentage");
         double c=sss.nextDouble();
         System.out.println("the value is "+ c);
-        
-        //for getting a character
+         //for getting a character
         char cv=sss.next().charAt(0);
         System.out.println(" the character is"+cv); */
         }
