fa892e9fa9609d274706690a556ce189b381e297
KARAN <110051623+Karanloganathan@users.noreply.github.com>
2023-01-11 22:26:21 +0530

    Create SolutionforioII.java

---
+++ SolutionforioII.java
@@ -0,0 +1,23 @@
+Hackerrank Java Stdin and Stdout II
+import java.util.Scanner;
+
+public class SolutionforioII {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int i = scan.nextInt();
+
+        // Write your code here.
+        
+        double d=scan.nextDouble();
+        scan.nextLine();
+        String s=scan.nextLine();
+
+        System.out.println("String: " + s);
+        System.out.println("Double: " + d);
+        System.out.println("Int: " + i);
+    }
+}
+
+
+
