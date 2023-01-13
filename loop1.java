a1ada6e863373f6f19e69a8f5984dc8a2a988c17
KARAN <110051623+Karanloganathan@users.noreply.github.com>
2023-01-13 23:01:00 +0530

    Create loop1.java

---
+++ loop1.java
@@ -0,0 +1,30 @@
+//Hackerrank solution for LOOP 1 question
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+
+
+public class loop1{
+    public static void main(String[] args) throws IOException {
+        Scanner sc=new Scanner(System.in);
+        int N=sc.nextInt();
+        
+        if(N>=2 && N<=20){
+            for(int i=1;i<=10;i++){
+                System.out.println(N+" x "+i+" = "+(N*i));
+            }
+        }
+        
+        sc.close();
+    }
+}
